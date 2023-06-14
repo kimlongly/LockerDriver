c333a067fc66f61d11913bfd6346b5c57167c502
Anuboost-Long <kimlongly57@gmail.com>
2023-06-14 12:09:36 +0700

    changes all the project problem:

--- android/app/src/main/java/com/lockerdriver/MainActivity.java
+++ android/app/src/main/java/com/lockerdriver/MainActivity.java
@@ -1,4 +1,4 @@
-package com.template;
+package com.lockerdriver;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "Template";
+    return "LockerDriver";
   }
 
   /**
